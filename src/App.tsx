cb666612ce1baa4968724d973beda1e2c0a0ca90
nanda-kshr <arrand59@gmail.com>
2024-12-03 20:00:27 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { EditorPage } from './pages/Editor';
 import { AuthPage } from './pages/Auth';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -12,8 +13,16 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/editor" element={<EditorPage />} />
+          <Route path="/" element={
+            <ProtectedRoute>
+            <Home />
+            </ProtectedRoute>
+          } />
+          <Route path="/editor" element={
+            <ProtectedRoute>
+            <EditorPage />
+            </ProtectedRoute>
+          }/>
           <Route path="/auth" element={<AuthPage />} />
         </Routes>
         <Toaster position="bottom-right" />
